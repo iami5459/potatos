cd247ff9a16bb9546f57cb521262ca3c1b3d1c9e
iami5459 <iami5459@colorado.edu>
2014-02-09 14:57:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Potatos::Application.config.secret_key_base = '8d68a5647eda4f2e074fe902e368932c1ebc6663ca0372c4930ae4096a17d1323ed89efdc9472c140d80caa870cf6dd34633f8fbe54f6aea13acbfb7c946d17f'
